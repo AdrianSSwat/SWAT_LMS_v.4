a1fc03d7e390a311d0fd924c61bd09159f081d66
AdrianSSwat <161358477+AdrianSSwat@users.noreply.github.com>
2024-03-31 17:33:07 -0400

    Added my project

--- components/professional/page.tsx
+++ components/professional/page.tsx
@@ -67,23 +67,6 @@ const professional = () => {
                     </ul>
                 </div>
             </div>
-            <div className='mt-12'>
-                <Link href='/msword' className="text-indigo-500 inline-flex items-center ml-6 lg:mb-0 items-left hover:underline">
-                    Learn More
-                    <svg
-                        className="w-4 h-4 ml-2"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                        fill="none"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                    >
-                        <path d="M5 12h14" />
-                        <path d="M12 5l7 7-7 7" />
-                    </svg>
-                </Link>
-            </div>
         </section>
     </>
   )
